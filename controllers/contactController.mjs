ba9af5127406fcebfc8855d3d25d88249306f2c2
Văn Nghĩa - Love <127735777+HVNN16@users.noreply.github.com>
2024-12-21 11:03:38 +0700

    again

--- controllers/contactController.mjs
+++ controllers/contactController.mjs
@@ -6,7 +6,6 @@ export const contactPage = (req, res) => {
 
 export const submitMessage = async (req, res) => {
     try {
-        // Lấy dữ liệu từ form
         const { name, email, subject, message } = req.body;
 
         const newMessage = new Message({
@@ -16,19 +15,15 @@ export const submitMessage = async (req, res) => {
             message
         });
 
-        // Lưu thông điệp vào MongoDB
         const savedMessage = await newMessage.save();
 
-        // Trả về phản hồi thành công
         res.status(200).json({
             success: true,
             message: 'Message sent successfully!'
         });
     } catch (error) {
-        // Log lỗi chi tiết
         console.error('Error details:', error);
 
-        // Trả về phản hồi lỗi
         res.status(500).json({
             success: false,
             message: 'Error sending message. Please try again.',
